5eaaefbad52716f9f0ea4f53ec1dd2ed510edf26
Gustavo <gustavo.brian@outlook.com>
2022-03-05 12:00:48 -0300

    att

--- src/route.js
+++ src/route.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const { get } = require('express/lib/response')
+const res = require('express/lib/response')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index'))
+route.get('/room', (req, res) => res.render('room'))
+route.get('/create-pass', (req, res) => res.render('create-pass'))
 
 module.exports = route
