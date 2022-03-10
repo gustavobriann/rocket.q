49b88f2a9cc49a82aed338720d0572a0f7b8c136
Gustavo <gustavo.brian@outlook.com>
2022-03-10 12:04:37 -0300

    att

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -44,10 +44,19 @@ module.exports = {
       `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
     )
 
+    let isNoQuestions
+
+    if (questions.length == 0) {
+      if (questionsRead.length == 0) {
+        isNoQuestions = true
+      }
+    }
+
     res.render('room', {
       roomId: roomId,
       questions: questions,
-      questionsRead: questionsRead
+      questionsRead: questionsRead,
+      isNoQuestions: isNoQuestions
     })
   },
 
